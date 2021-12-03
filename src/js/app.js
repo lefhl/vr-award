f7aed3c2838b68ee70fec81a588dffdbec497ece
edik <gizbreht.e@yandex.ru>
2021-12-03 19:57:53 +0300

    #5 Причесал десктоп, добавил эффектов

--- src/js/app.js
+++ src/js/app.js
@@ -2,10 +2,30 @@ import lozad from "lozad";
 import "../scss/style.scss";
 
 window.onload = function () {
+  document.body.classList.remove("transition-lock");
+
   lozad(".lozad", {
     rootMargin: "40px 0px",
     loaded: function (el) {
       el.classList.add("loaded");
     },
   }).observe();
+
+  lozad(".anim", {
+    rootMargin: "40px 0px",
+    loaded: function (el) {
+      el.classList.add("loaded");
+    },
+  }).observe();
+
+  // document.querySelectorAll(".anim").forEach((item) => {
+  //   item.addEventListener(
+  //     "animationend",
+  //     function () {
+  //       const $el = this;
+  //       $el.classList.add("loaded");
+  //     },
+  //     { once: true }
+  //   );
+  // });
 };
