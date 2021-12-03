29a8c018ee9012bb92ef9ce1ed5ed9f61fe6566e
edik <gizbreht.e@yandex.ru>
2021-12-03 21:58:24 +0300

    #6 Часть адаптива + мобильное меню

--- src/js/app.js
+++ src/js/app.js
@@ -18,14 +18,25 @@ window.onload = function () {
     },
   }).observe();
 
-  // document.querySelectorAll(".anim").forEach((item) => {
-  //   item.addEventListener(
-  //     "animationend",
-  //     function () {
-  //       const $el = this;
-  //       $el.classList.add("loaded");
-  //     },
-  //     { once: true }
-  //   );
-  // });
+  document.querySelector(".burger").addEventListener(
+    "click",
+    function () {
+      document.querySelector(".header__list").classList.toggle("js-active");
+      this.classList.toggle("js-active");
+      document.body.classList.toggle("scroll-lock");
+    },
+    { passive: true }
+  );
+
+  document.querySelectorAll(".header__link").forEach((item) => {
+    item.addEventListener(
+      "click",
+      function () {
+        document.querySelector(".burger").classList.remove("js-active");
+        document.body.classList.remove("scroll-lock");
+        document.querySelector(".header__list").classList.remove("js-active");
+      },
+      { passive: true }
+    );
+  });
 };
